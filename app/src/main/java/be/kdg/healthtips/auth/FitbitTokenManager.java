0ba32869a77136d8c63b07731d8b1f3b069cdca0
Steven Goris <steven.goris@student.kdg.be>
2015-02-01 21:39:44 +0100

    added tokenmanager

---
+++ app/src/main/java/be/kdg/healthtips/auth/FitbitTokenManager.java
@@ -0,0 +1,58 @@
+package be.kdg.healthtips.auth;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class FitBitTokenManager {
+    private static final String CONSUMER_KEY = "0dc58a7d5b1349a187b74e6e82d989f5";
+    private static final String CONSUMER_SECRET = "2d234d453df949a786971a9253a22f99";
+    private String fitBitAccesToken;
+    private String fitBitAccesTokenSecret;
+    private static FitBitTokenManager instance;
+    private SharedPreferences sharedPreferences;
+
+    public FitBitTokenManager(Context context) {
+        this.sharedPreferences = context.getSharedPreferences("keys", Context.MODE_MULTI_PROCESS);
+    }
+
+    public static String getConsumerKey() {
+        return CONSUMER_KEY;
+    }
+
+    public static String getConsumerSecret() {
+        return CONSUMER_SECRET;
+    }
+
+    public void setFitBitAccesToken(String fitBitAccesToken) {
+        this.fitBitAccesToken = fitBitAccesToken;
+
+        SharedPreferences.Editor sharedPreferencesEditor = sharedPreferences.edit();
+        sharedPreferencesEditor.putString("FitbitAccessToken", fitBitAccesToken);
+        sharedPreferencesEditor.apply();
+    }
+
+    public void setFitBitAccesTokenSecret(String fitBitAccesTokenSecret) {
+        this.fitBitAccesTokenSecret = fitBitAccesTokenSecret;
+
+        SharedPreferences.Editor sharedPreferencesEditor = sharedPreferences.edit();
+        sharedPreferencesEditor.putString("FitbitAccessTokenSecret", fitBitAccesTokenSecret);
+        sharedPreferencesEditor.apply();
+    }
+
+    public String getFitBitAccesToken() {
+        if (fitBitAccesToken == null || fitBitAccesToken.isEmpty())
+            fitBitAccesToken = sharedPreferences.getString("FitbitAccessToken", "");
+        return fitBitAccesToken;
+    }
+
+    public String getFitBitAccesTokenSecret() {
+        if (fitBitAccesTokenSecret == null || fitBitAccesTokenSecret.isEmpty())
+            fitBitAccesTokenSecret = sharedPreferences.getString("FitbitAccessTokenSecret", "");
+        return fitBitAccesTokenSecret;
+    }
+
+    public static FitBitTokenManager getInstance(Context context) {
+        if (instance == null) instance = new FitBitTokenManager(context);
+        return instance;
+    }
+}

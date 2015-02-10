924c605579380b736c6193c7d6ef6b09f495bd1a
Steven Goris <sg.steven13@gmail.com>
2015-02-11 00:04:32 +0100

    Translated some notifications to dutch. Finetuning home_layout.

--- app/src/main/java/be/kdg/healthtips/notifications/TipManager.java
+++ app/src/main/java/be/kdg/healthtips/notifications/TipManager.java
@@ -7,39 +7,39 @@ import java.util.Random;
 import be.kdg.healthtips.activity.TipDetailActivity;
 
 public class TipManager {
-    public static void throwRandomStepTip(String reden, Context context) {
-        throwTipOfSubject("steps", reden, "Get more steps", context, NotificationThrower.IconType.T_STEPS);
+    public static void throwRandomStepTip(String reason, Context context) {
+        throwTipOfSubject("steps", reason, "Doe meer stappen", context, NotificationThrower.IconType.T_STEPS);
     }
 
-    public static void throwRandomRunningTip(String reden, Context context) {
-        throwTipOfSubject("running", reden, "Run more efficiently", context, NotificationThrower.IconType.T_RUNNING);
+    public static void throwRandomRunningTip(String reason, Context context) {
+        throwTipOfSubject("running", reason, "Loop vaker", context, NotificationThrower.IconType.T_RUNNING);
     }
 
-    public static void throwRandomFallingASleepTip(String reden, Context context) {
-        throwTipOfSubject("sleep", reden, "Fall asleep faster", context, NotificationThrower.IconType.T_SLEEP);
+    public static void throwRandomFallingASleepTip(String reason, Context context) {
+        throwTipOfSubject("sleep", reason, "Val sneller in slaap", context, NotificationThrower.IconType.T_SLEEP);
     }
 
-    public static void throwRandomSleepTip(String reden, Context context) {
-        throwTipOfSubject("sleep", reden, "Sleep better", context, NotificationThrower.IconType.T_SLEEP);
+    public static void throwRandomSleepTip(String reason, Context context) {
+        throwTipOfSubject("sleep", reason, "Slaap beter", context, NotificationThrower.IconType.T_SLEEP);
     }
 
-    public static void throwRandomGewichtTip(String reden, Context context) {
+    public static void throwRandomGewichtTip(String reason, Context context) {
         Random r = new Random();
         if (r.nextBoolean()) {
-            throwRandomFoodTip(reden, context);
+            throwRandomFoodTip(reason, context);
         } else {
-            throwTipOfSubject("food", reden, "Loose weight", context, NotificationThrower.IconType.T_WEIGHT);
+            throwTipOfSubject("food", reason, "Verlies gewicht", context, NotificationThrower.IconType.T_WEIGHT);
         }
     }
 
-    public static void throwRandomFoodTip(String reden, Context context) {
-        throwTipOfSubject("food", reden, "Eat Healthy", context, NotificationThrower.IconType.T_FOOD);
+    public static void throwRandomFoodTip(String reason, Context context) {
+        throwTipOfSubject("food", reason, "Eet gezonder", context, NotificationThrower.IconType.T_FOOD);
     }
 
-    private static void throwTipOfSubject(String subject, String reden, String titel, Context context, NotificationThrower.IconType typeIcon) {
+    private static void throwTipOfSubject(String subject, String reason, String title, Context context, NotificationThrower.IconType typeIcon) {
         TipGetter tipGetter = new TipGetter();
         int tipnr = tipGetter.getRandomTipNr(subject, context);
-        NotificationThrower.throwNotification(context, typeIcon, titel, reden, TipDetailActivity.class, tipnr);
+        NotificationThrower.throwNotification(context, typeIcon, title, reason, TipDetailActivity.class, tipnr);
     }
 
 

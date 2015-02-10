69013dfa8930c79ab711233ac999f239474e8e92
joni <joni.verzelen@student.kdg.be>
2015-02-10 22:49:16 +0100

    nieuw prentje

--- app/src/main/java/be/kdg/healthtips/activity/EtenActivity.java
+++ app/src/main/java/be/kdg/healthtips/activity/EtenActivity.java
@@ -27,7 +27,6 @@ import be.kdg.healthtips.task.GetDataATask;
 public class EtenActivity extends Activity {
     private Context context;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
